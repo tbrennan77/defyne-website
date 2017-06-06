3f7acb578081a409723d75ba265ce30833279853
Timothy Brennan <2timothy215@gmail.com>
2017-06-06 13:28:00 -0400

    Mega Menu added

--- js/main.js
+++ js/main.js
@@ -93,3 +93,18 @@ if ( typeof define === 'function' && define.amd ) {
 
 }(window));
 
+
+
+$(document).ready(function(){
+    $(".dropdown").hover(            
+        function() {
+            $('.dropdown-menu', this).not('.in .dropdown-menu').stop(true,true).slideDown("400");
+            $(this).toggleClass('open');        
+        },
+        function() {
+            $('.dropdown-menu', this).not('.in .dropdown-menu').stop(true,true).slideUp("400");
+            $(this).toggleClass('open');       
+        }
+    );
+});
+
