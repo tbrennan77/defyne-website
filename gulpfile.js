77ae236cb85505f2ce268648764812f68b91a6d2
Timothy Brennan <2timothy215@gmail.com>
2017-06-19 14:03:48 -0400

    gulpfile compile sass added

--- gulpfile.js
+++ gulpfile.js
@@ -3,9 +3,17 @@ var gulp = require('gulp');
 // Requires the gulp-sass plugin
 var sass = require('gulp-sass');
 
+gulp.task("default", function () {
+  gulp.start("sass");
+});
+
 gulp.task('sass', function(){
   return gulp.src('app/scss/styles.scss')
     .pipe(sass()) // Using gulp-sass
     .pipe(gulp.dest('app/css'))
 });
 
+
+gulp.task('watch', function(){
+  gulp.watch('app/scss/**/*.scss', ['sass']); 
+})
