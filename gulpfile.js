16a6c56044dd52f11b03ba2bc91a19657d3fb447
Timothy Brennan <2timothy215@gmail.com>
2017-06-19 14:33:07 -0400

    gulp useref added

--- gulpfile.js
+++ gulpfile.js
@@ -2,7 +2,12 @@
 var gulp = require('gulp');
 // Requires the gulp-sass plugin
 var sass = require('gulp-sass');
+// Requires BrowserSync
+var browserSync = require('browser-sync').create();
+// Require UserRef
+var useref = require('gulp-useref');
 
+// Run sass by defuault
 gulp.task("default", function () {
   gulp.start("sass");
 });
@@ -11,9 +16,23 @@ gulp.task('sass', function(){
   return gulp.src('app/scss/styles.scss')
     .pipe(sass()) // Using gulp-sass
     .pipe(gulp.dest('app/css'))
+    .pipe(browserSync.reload({
+      stream: true
+    }))
 });
 
-
-gulp.task('watch', function(){
+// Gulp Watch
+gulp.task('watch', ['browserSync', 'sass'], function(){
   gulp.watch('app/scss/**/*.scss', ['sass']); 
+  gulp.watch('*.php', browserSync.reload); 
+  gulp.watch('app/js/**/*.js', browserSync.reload); 
+})
+
+// BrowserSync
+gulp.task('browserSync', function() {
+  browserSync.init({
+    server: {
+      baseDir: 'app'
+    },
+  })
 })
