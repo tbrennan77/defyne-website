e72376b19766806667dacefaa1720a61d36f8bf7
Timothy Brennan <2timothy215@gmail.com>
2017-06-29 12:33:12 -0400

    Add active class for menu links

--- inc/page-menu.php
+++ inc/page-menu.php
@@ -1,3 +1,8 @@
+<?php 
+	$page = $_SERVER['PHP_SELF']; 
+	$page = str_replace(".php","",$page);
+	$page = trim(str_replace("/","",$page));
+?>   
 <nav class="navbar navbar-toggleable-md navbar-inverse fixed-top bg-inverse">
 	<div class="container force-mobile-full">
 		<div class="row w-100">
@@ -11,19 +16,19 @@
 		    </div>
 		    <div class="collapse navbar-collapse col-lg-8 col-sm-12 col-xs-12 pl-0 pr-0 ml-xs-4" id="navbarCollapse">
 		        <ul class="navbar-nav">
-		          <li class="nav-item active">
+		          <li class="nav-item <?php echo ($page == "index" ? "active" : "")?>">
 		            <a class="nav-link hvr-pulse" href="/">Home <span class="sr-only">(current)</span></a>
 		          </li>
-		          <li class="nav-item">
+		          <li class="nav-item <?php echo ($page == "about" ? "active" : "")?>">
 		            <a class="nav-link hvr-pulse" href="/about">About</a>
 		          </li>
-		          <li class="nav-item">
+		          <li class="nav-item <?php echo ($page == "partners" ? "active" : "")?>">
 		            <a class="nav-link hvr-pulse" href="/partners">Partners</a>
 		          </li>
-		          <li class="nav-item">
+		          <li class="nav-item <?php echo ($page == "products" ? "active" : "")?>">
 		            <a class="nav-link hvr-pulse" href="/products">Products</a>
 		          </li>
-		          <li class="nav-item">
+		          <li class="nav-item <?php echo ($page == "contact" ? "active" : "")?>">
 		            <a class="nav-link hvr-pulse" href="/contact">Contact</a>
 		          </li>
 		        </ul>
