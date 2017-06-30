c0130e45ec151fd7c163116cf437511dd8368d7c
Timothy Brennan <2timothy215@gmail.com>
2017-06-30 12:24:11 -0400

    Partners page added

---
+++ partners.php
@@ -0,0 +1,175 @@
+<head>
+	<meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <link rel="icon" href="favicon.ico">
+
+    <title>Defyne Payments | Defyne Your Success</title>
+
+    <?php include ("inc/header.php"); ?>
+
+    <link rel="stylesheet" href="css/about.css" />
+
+</head>
+<body class="about">
+
+	<!-- Footer Menu Row -->
+	<?php include ("inc/page-menu.php"); ?>
+    <!-- /.container -->
+
+    <!-- Jumbotron section -->
+	<div class="jumbotron jumbotron-fluid" style="background-image: url('img/group-94-2.png');">
+	  <div class="container">
+	  	<div class="row">
+	  		<div class="col-sm-12 col-lg-6 p-4">
+			    <p class="lead">Put the best of payments to work for you with one powerful platform<br /> - it’s Defying & Defining</p>
+			    <button class="btn btn-default hvr-grow">Let's Talk</button>
+			</div>
+			<div class="hidden-md-down col-lg-6 p-4">
+				<img src="img/group-63.png" srcset="img/group-63@2x.png 2x, img/group-63@3x.png 3x" class="img-fluid" />
+			</div>
+	    </div>
+	  </div>
+	</div><!-- /.container -->
+
+    <!-- Column Row -->
+    <div class="container">
+    	<div class="row">
+    		<div class="col-sm-12 text-center pt-4 pb-4">
+    			<img src="img/logo.png" srcset="img/logo@2x.png 2x, img/logo@3x.png 3x" class="logo img-fluid" />
+    			<h2 class="pt-4 pb-4 blue-h2">Defyne Payments was created in April 2017 with one vision - to merge the mindset of new technology companies entering the payments industry with the service, support and partnership that only the best of the best companies can deliver. By merging these business philosophies we are doing one thing: Re-Defyning Payments. Simple, Smooth, Transparent, Fast.</p>
+    		</div>
+    	</div>
+    </div><!-- /.container -->
+
+    <!-- Team Container -->
+	<div class="container">
+	    <div class="row">
+	    	<div class="col-xss-12 col-lg-12 text-center blue pt-4">
+	    		<h1 class="blue">Executive Leadership</h1>
+	    		<p class="grey-text pb-4">Our management team was carefully crafted to bring significant payments know how with free thinking minds that create new solutions by keeping our customers and their needs first. Our team’s track record of success is unparalleled.</p>
+	    	</div>
+	    	
+	    	<div class="col-md-12 col-xs-12 col-xss-12">
+	            <div class="leadership duayne">
+					<div class="leaderline cf">
+						<img src="img/duayne_haskett_ceo.png">
+						<div class="exec">
+							<div class="exec-name">Duayne Haskett</div>
+							<div class="exec-title">Chief Executive Officer</div>
+						</div>
+						<div class="toggle-bio duayne"></div>
+						<div class="bio duayne" style="display: none;">
+							<p>Duayne Haskett has been a driving force in merchant acquiring since leaving the issuing side of the business in 2001. After working large financial institutions such as Washington Mutual and Wells Fargo early in his career, Duayne entered the Merchant Acquiring arena. Duayne served as a leader in Credit Underwriting and Operations for Sage Payments (formerly Verus Card Services) through September 2005.</p>
+							<p>Mr. Haskett departed to become a founding member of the management team at Priority Payment Systems (Priority). He then proceeded to manage and develop one of the merchant processing industries premier ISO/Agent and VAR/SAS channels for 11 years. Upon his departure from Priority, Duayne determined to bring back a customer centric infrastructure to the new era of payment technology.</p>
+						</div>
+					</div>
+				</div>
+			</div>
+
+	        <div class="col-md-12 col-xs-12 col-xss-12">
+	            <div class="leadership stephen">
+					<div class="leaderline cf">
+						<img src="img/stephen_palazzo_coo.png">
+						<div class="exec">
+							<div class="exec-name">Stephen Palazzo</div>
+							<div class="exec-title">Chief Operating Officer</div>
+						</div>
+						<div class="toggle-bio stephen"></div>
+						<div class="bio stephen" style="display: none;">
+							<p>Stephen Palazzo has been a critical management team member for many payment industry providers over the past 30 years.  Mr. Palazzo has built industry leading operational infrastructure in roles at some of the worlds largest acquirers. In addition to his operations expertise and experience, Stephen has also been tasked with leading important growth and technology orientated programs within First Data, Chase Bank, Global Payments, Elavon, WorldPay, and most recently Sage Payments.</p>
+							<p>As the leader of Sales within the ISO and Agent channel of Sage he pushed forth several integrated technology initiatives into the channel. He has received a Bachelor of Science degree in business from The University of the State of New York.  As a founder of Defyne Payments, Steve is looking to bring a new and fresh take on the operational environment to lead change and revolution within the electronics payments space.</p>
+						</div>
+					</div>
+				</div>
+	        </div><!-- End .col-md-3 -->
+	    
+	        <div class="col-md-12 col-xs-12 col-xss-12">
+	            <div class="leadership bryan">
+					<div class="leaderline cf">
+						<img src="img/bryan_hinkle_cto.png">
+						<div class="exec">
+							<div class="exec-name">Bryan Hinkle</div>
+							<div class="exec-title">Chief Technology Officer</div>
+						</div>
+						<div class="toggle-bio bryan"></div>
+						<div class="bio bryan" style="display: none;">
+							 <p>Bryan Hinkle has developed several back-office management and web based platforms since 2002. Bryan’s college consisted of GCSU MIS degree and Auburn University LA/CS.   Bryan has spent time as part of a production development team servicing multiple aspects of Total Systems (TSYS), including issuing card production and share-point dashboard systems for management reporting. </p>
+
+							 <p>Mr. Hinkle then joined Scientific Games in Alpharetta Georgia as their Senior Programming Analyst and Web Services Product Manager. For 11 years Bryan and his team designed and programmed the accounting and customized ERP and CRM systems that manage all state lottery tickets being produced.  Bryan has spent time internationally implementing his systems providing global leadership in ticketing software. As a Founder of Defyne Payments, Bryan is determined to build payments software that is unparalleled in the market today to lead the new electronic payments eco-system.</p>
+						</div>
+					</div>	  
+				</div>      
+	        </div><!-- End .col-md-3 -->
+	    </div>
+	</div> 
+
+	<!-- Map Container -->
+	<div class="container-fluid">
+		<div class="row">
+			<div class="col-12">
+				<div id="map"></div>
+			</div>
+		</div>
+	</div>
+
+	<div id="map-canvas"></div>
+
+    <!-- Footer Menu Row -->
+	<?php include ("inc/footer-menu.php"); ?>
+
+    <!-- Footer Small Print Row -->
+    <?php include ("inc/footer.php"); ?>
+
+    <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyAopcy5lD5GZUX1X0zGIdiOdsGYStvoyVk"></script>
+    <script>
+		$(document).ready(function() {
+
+		var map;
+		var location = new google.maps.LatLng(34.3788845, -84.0114542);
+
+		var mapCanvas = document.getElementById('map-canvas');
+        var mapOptions = {
+            center: location,
+            zoom: 8,
+            panControl: false,
+            mapTypeId: google.maps.MapTypeId.ROADMAP
+        }
+        var map = new google.maps.Map(mapCanvas, mapOptions);
+
+		var markerImage = 'img/marker.png';
+
+        var marker = new google.maps.Marker({
+            position: location,
+            map: map,
+            icon: markerImage
+        });
+
+        var contentString = '<div class="info-window">' +
+                '<h3>Info Window Content</h3>' +
+                '<div class="info-content">' +
+                '<p>Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Vestibulum tortor quam, feugiat vitae, ultricies eget, tempor sit amet, ante. Donec eu libero sit amet quam egestas semper. Aenean ultricies mi vitae est. Mauris placerat eleifend leo.</p>' +
+                '</div>' +
+                '</div>';
+
+        var infowindow = new google.maps.InfoWindow({
+            content: contentString,
+            maxWidth: 400
+        });
+
+        marker.addListener('click', function () {
+            infowindow.open(map, marker);
+        });
+
+		var styles = [ { "featureType": "administrative.land_parcel", "elementType": "all", "stylers": [ { "visibility": "off" } ] }, { "featureType": "landscape.man_made", "elementType": "all", "stylers": [ { "visibility": "off" } ] }, { "featureType": "poi", "elementType": "labels", "stylers": [ { "visibility": "off" } ] }, { "featureType": "road", "elementType": "labels", "stylers": [ { "visibility": "simplified" }, { "lightness": 20 } ] }, { "featureType": "road.highway", "elementType": "geometry", "stylers": [ { "hue": "#f49935" } ] }, { "featureType": "road.highway", "elementType": "labels", "stylers": [ { "visibility": "simplified" } ] }, { "featureType": "road.arterial", "elementType": "geometry", "stylers": [ { "hue": "#fad959" } ] }, { "featureType": "road.arterial", "elementType": "labels", "stylers": [ { "visibility": "off" } ] }, { "featureType": "road.local", "elementType": "geometry", "stylers": [ { "visibility": "simplified" } ] }, { "featureType": "road.local", "elementType": "labels", "stylers": [ { "visibility": "simplified" } ] }, { "featureType": "transit", "elementType": "all", "stylers": [ { "visibility": "off" } ] }, { "featureType": "water", "elementType": "all", "stylers": [ { "hue": "#a1cdfc" }, { "saturation": 30 }, { "lightness": 49 } ] } ];
+
+		map.set('styles', styles);
+
+		google.maps.event.addDomListener(window, 'load', initialize);
+
+		});
+    </script>
+
+</body>
